dc74aeca10fef0f83e46e5a3eea47adf5ca3d953
Muhammad Rayhan Hamada Budiman <rayhanhamada1999@gmail.com>
2022-08-07 18:41:36 +0700

    feat: add secureU text

--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:secureu_mobile/screens/splash/splash.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
@@ -6,10 +8,27 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var theme = Theme.of(context);
+    final splashBloc = context.watch<SplashBloc>()
+      ..add(const SplashEvent.started());
 
     return Scaffold(
       backgroundColor: theme.backgroundColor,
-      body: Container(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: const [
+          Center(
+            child: Text(
+              'SecureU',
+              style: TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 64,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
