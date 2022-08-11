9feba4609ae17cb95cff0c67f3224f3b6fe6e842
Muhammad Rayhan Hamada Budiman <rayhanhamada1999@gmail.com>
2022-08-11 09:29:57 +0700

    feat: add register screen

---
+++ lib/screens/register/register_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatelessWidget {
+  const RegisterScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Scaffold(
+      backgroundColor: theme.backgroundColor,
+      // TODO: implement appbar
+      appBar: null,
+
+      // TODO: implement register body
+      body: null,
+    );
+  }
+}
