8da3caa370176ba467eada5ad93dc6c43495ce90
Muhammad Rayhan Hamada Budiman <rayhanhamada1999@gmail.com>
2022-08-16 16:32:04 +0700

    feat: improve login

--- lib/screens/login/bloc/login_bloc.dart
+++ lib/screens/login/bloc/login_bloc.dart
@@ -1,16 +1,18 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:secureu_mobile/repos/account_repository.dart';
+import 'package:secureu_mobile/utils/cryptography.dart';
 
 part 'login_event.dart';
 part 'login_state.dart';
 part 'login_bloc.freezed.dart';
 
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
-  LoginBloc({required AccountRepository accountRepo})
-      : super(const _Initial()) {
-    accountRepo;
+  final AccountRepository _accountRepo;
 
+  LoginBloc({required AccountRepository accountRepo})
+      : _accountRepo = accountRepo,
+        super(const _Initial()) {
     on<_Started>((event, emit) {
       print('you\'ve entered login page');
     });
@@ -18,10 +20,39 @@ class LoginBloc extends Bloc<LoginEvent, LoginState> {
     on<_SubmitLogin>((event, emit) async {
       emit(const LoginState.submittingLogin());
 
-      // TODO: implement login (replace LoginState.initial with LoginState.successLogin)
-      await Future<void>.delayed(
-        const Duration(seconds: 3),
+      // get matched account
+      final account = await _accountRepo.getAccountByEmail(event.email);
+
+      if (account == null) {
+        return emit(const LoginState.failedLogin('Akun tidak ditemukan'));
+      }
+
+      final accountPassword = account.password;
+
+      // derive password to pbkdf2 string
+      final derivedPassword = await Cryptography.passwordToBase64HKDFString(
+        masterPassword: event.password,
+        email: event.email,
       );
+
+      // if there's any error when processing password
+      if (derivedPassword == null) {
+        return emit(
+          const LoginState.failedLogin('Kesalahan saat memproses password'),
+        );
+      }
+
+      if (accountPassword != derivedPassword) {
+        return emit(const LoginState.failedLogin('Kesalahan saat login'));
+      }
+
+      // turunkan master password menjadi string PBKDF2 base64
+      final derivedInputPassword = Cryptography.passwordToBase64PBKDF2String(
+        email: event.email,
+        masterPassword: event.password,
+      );
+
+      // TODO: implement hive/shared_preferences to save derived input password
 
       return emit(const LoginState.successLogin());
     });
--- lib/utils/cryptography.dart
+++ lib/utils/cryptography.dart
@@ -59,4 +59,35 @@ class Cryptography {
 
     return base64StretchedMasterKey;
   }
+
+  static Future<String?> passwordToBase64PBKDF2String({
+    required String masterPassword,
+    required String email,
+  }) async {
+    // Definisikan algoritma PBKDF2 dengan menggunakan
+    // Pseduorandom Function HMAC-SHA256 dengan iterasi
+    // sebanyak 100000 kali, yang menghasilkan array 32 byte
+    final pbkdf2Algorithm = Pbkdf2(
+      macAlgorithm: Hmac.sha256(),
+      iterations: 100000,
+      bits: 256,
+    );
+
+    // Kodekan untai master password dan email kedalam kode UTF-8
+    final utf8EncodedMasterPassword = utf8.encode(masterPassword);
+    final utf8EncodedEmail = utf8.encode(email);
+
+    // Turunkan master password menggunakan algoritma PBKDF2
+    // yang telah didefinisikan, dengan salt berupa email.
+    // Nilai dari key ini akan menjadi kunci enkripsi data
+    // akun pengguna.
+    final masterKey = await pbkdf2Algorithm.deriveKey(
+      secretKey: SecretKey(utf8EncodedMasterPassword),
+      nonce: utf8EncodedEmail,
+    );
+
+    final masterKeyBytes = await masterKey.extractBytes();
+
+    return base64.encode(masterKeyBytes);
+  }
 }
