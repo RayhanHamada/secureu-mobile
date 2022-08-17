9d11db08e45479c6ea12757b274d3965df293c5e
Muhammad Rayhan Hamada Budiman <rayhanhamada1999@gmail.com>
2022-08-17 16:15:39 +0700

    feat: add more routes

--- lib/secureu_app.dart
+++ lib/secureu_app.dart
@@ -6,9 +6,13 @@ import 'package:secureu_mobile/repos/account_repository.dart';
 import 'package:secureu_mobile/screens/create_secret/bloc/create_secret_bloc.dart';
 import 'package:secureu_mobile/screens/create_secret/create_secret.dart';
 import 'package:secureu_mobile/screens/dashboard/dashboard.dart';
+import 'package:secureu_mobile/screens/edit_secret/bloc/edit_secret_bloc.dart';
+import 'package:secureu_mobile/screens/edit_secret/edit_secret.dart';
 import 'package:secureu_mobile/screens/login/login.dart';
 import 'package:secureu_mobile/screens/register/register.dart';
 import 'package:secureu_mobile/screens/splash/splash.dart';
+import 'package:secureu_mobile/screens/view_secret/bloc/view_secret_bloc.dart';
+import 'package:secureu_mobile/screens/view_secret/view_secret.dart';
 
 class SecureUApp extends StatelessWidget {
   const SecureUApp({Key? key}) : super(key: key);
@@ -18,6 +22,7 @@ class SecureUApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: SecureUTheme.of(context),
+      initialRoute: SecureURoutes.splash,
       routes: <String, WidgetBuilder>{
         SecureURoutes.splash: (context) => BlocProvider(
               create: (context) => SplashBloc(),
@@ -43,8 +48,15 @@ class SecureUApp extends StatelessWidget {
               create: (context) => CreateSecretBloc(),
               child: const CreateSecretScreen(),
             ),
+        SecureURoutes.viewSecret: (context) => BlocProvider(
+              create: (_) => ViewSecretBloc(),
+              child: const ViewSecretScreen(),
+            ),
+        SecureURoutes.editSecret: (context) => BlocProvider(
+              create: (_) => EditSecretBloc(),
+              child: const EditSecretScreen(),
+            ),
       },
-      initialRoute: SecureURoutes.splash,
     );
   }
 }
