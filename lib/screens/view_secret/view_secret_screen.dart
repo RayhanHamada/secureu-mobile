83cf1ae626e8423011fa349f564292c241c3d65e
Muhammad Rayhan Hamada Budiman <rayhanhamada1999@gmail.com>
2022-08-22 17:34:26 +0700

    feat: add view_secret screen appbar and body

--- lib/screens/view_secret/view_secret_screen.dart
+++ lib/screens/view_secret/view_secret_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:secureu_mobile/config/routes.dart';
+import 'package:secureu_mobile/screens/view_secret/bloc/view_secret_bloc.dart';
 
 class ViewSecretScreen extends StatelessWidget {
   const ViewSecretScreen({Key? key}) : super(key: key);
@@ -7,8 +10,45 @@ class ViewSecretScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
 
-    return Scaffold(
-      backgroundColor: theme.scaffoldBackgroundColor,
+    return BlocListener<ViewSecretBloc, ViewSecretState>(
+      listener: (context, state) {},
+      child: Scaffold(
+        backgroundColor: theme.scaffoldBackgroundColor,
+        appBar: _appbar(context),
+        body: _body(context),
+      ),
+    );
+  }
+
+  PreferredSizeWidget _appbar(BuildContext context) {
+    return AppBar(
+      title: const Text('View Secret'),
+      centerTitle: true,
+      leading: IconButton(
+        onPressed: () {
+          Navigator.of(context).pop();
+        },
+        icon: const Icon(
+          Icons.arrow_back_ios,
+        ),
+      ),
+      actions: [
+        IconButton(
+          onPressed: () {
+            Navigator.pushNamed(context, SecureURoutes.editSecret);
+          },
+          icon: const Icon(
+            Icons.edit_outlined,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget _body(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: const [],
     );
   }
 }
