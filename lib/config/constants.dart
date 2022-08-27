4d165a0cc84da2b7ed5ce8c6bec954e2942c6ea1
Muhammad Rayhan Hamada Budiman <rayhanhamada1999@gmail.com>
2022-08-27 22:24:46 +0700

    feat: add for mobile

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -1,3 +1,3 @@
 const isProduction = bool.fromEnvironment('dart.vm.product');
-final pocketbaseURL = Uri.parse('http://localhost:8090/');
-const hivePath = 'hive_datas';
+final pocketbaseURL = Uri.parse('http://103.139.192.196:8090/');
+const hivePath = null;
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,22 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
 import 'package:hive/hive.dart';
 import 'package:secureu_mobile/config/constants.dart' as constants;
 import 'package:secureu_mobile/config/hive_constants.dart';
 import 'package:secureu_mobile/secureu_app.dart';
+import 'package:path_provider/path_provider.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
   if (!constants.isProduction) {
     // jika development environment
 
     print('Development mode');
+    Directory directory = await getApplicationDocumentsDirectory();
 
-    Hive.init(constants.hivePath);
+    Hive.init(directory.path);
 
     await Hive.openBox<String>(HiveConstants.appsession);
   } else {
